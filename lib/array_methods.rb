00b56d74120db478453687e34203c566734e1338
Malcom Fetus <sora.vista@gmail.com>
2020-02-17 15:37:33 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def using_include(array, element)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
-array.include?(array, "wow")
-array.include?(array, "Tardis")
+array.include?("element1")
+array.include?("element2")
 
 end
 
